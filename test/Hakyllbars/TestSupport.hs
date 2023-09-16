1a88b1324d4272dc3d09c58447b52a98c22b379b
Logan McGrath <logan.mcgrath@thisfieldwas.green>
2023-09-16 15:15:55 -0700

    Stripping unused test stuff

--- test/Hakyllbars/TestSupport.hs
+++ test/Hakyllbars/TestSupport.hs
@@ -1,16 +1,8 @@
 module Hakyllbars.TestSupport
   ( module Hakyllbars.Common,
-    module Hakyllbars.TestSupport.Compiler,
-    module Hakyllbars.TestSupport.Config,
-    module Hakyllbars.TestSupport.Resource,
-    module Hakyllbars.TestSupport.TestEnv,
     module Test.Hspec,
   )
 where
 
 import Hakyllbars.Common
-import Hakyllbars.TestSupport.Compiler
-import Hakyllbars.TestSupport.Config
-import Hakyllbars.TestSupport.Resource
-import Hakyllbars.TestSupport.TestEnv
 import Test.Hspec
